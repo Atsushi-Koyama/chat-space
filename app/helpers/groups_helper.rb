2e69035b190c3bcab5ce12f97e877bf955167013
Atsushi-Koyama <two51ct13th@yahoo.co.jp>
2018-01-12 14:02:50 +0900

    edit-helper

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,11 @@
 module GroupsHelper
+
+  def show_last_message(group)
+    if (last_message = group.messages.last).present?
+      last_message.content? ? last_message.content : '画像が投稿されています'
+    else
+      'まだメッセージはありません。'
+    end
+  end
+
 end
